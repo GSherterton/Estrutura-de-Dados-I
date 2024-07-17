671137c5224427903c46ffdba56e09fb3366c31c
GSHerterton <gsherterton@gmail.com>
2024-07-17 15:31:30 -0300

    adicionando o algoritmo do selection sort

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -28,11 +28,35 @@ struct Instance{
             vec.push_back(aux);
         }
     }
+
+    void print(){
+        for(int i = 0; i < vec.size(); i++){
+            cout << vec[i] << " ";
+        }cout << "\n";
+    }
 };
 
-void selectionSort(vector<int> vec){
-    for(int i = 0; i < 100000; i++){
+void troca(int &a, int &b){
+    int aux;
+    aux = a;
+    a = b;
+    b = aux;
+}
 
+void selectionSort(vector<int>& vec){
+    int tamanho = vec.size()-1;
+
+    int *menor;
+    int i, j;
+    for(i = 0; i < tamanho; i++){ //itera sobre o vetor
+        menor = &vec[i];
+        for(j = i+1; j <= tamanho; j++){ //procura o menor elemento a partir da posicao i
+            if(vec[j] < *menor){
+                menor = &vec[j];
+            }
+        }
+
+        troca(vec[i], *menor); //passa o elemento de menor e da iteração atual
     }
 }
 
@@ -51,11 +75,17 @@ int main(int argc, char** argv){
     clock_t start, end;
     double tempo = 0;
 
+    //debug
+    //instance.print();
+
     start = clock(); //inicia o contador de tempo
 
     selectionSort(instance.vec);
 
     end = clock(); //pega o tempo final
+
+    //debug
+    //instance.print();
 
     tempo = (double)(end - start) / (double)(CLOCKS_PER_SEC); //realiza o cálculo do tempo gasto pela ordenação
 
